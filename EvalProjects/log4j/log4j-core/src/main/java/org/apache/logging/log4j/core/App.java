72b100104190b9533cf0a046dd767951d2bf75b3
Kooper16 <56591765+Kooper16@users.noreply.github.com>
2024-06-04 23:56:39 +0200

    log4j working but only without gdart

--- EvalProjects/log4j/log4j-core/src/main/java/org/apache/logging/log4j/core/App.java
+++ EvalProjects/log4j/log4j-core/src/main/java/org/apache/logging/log4j/core/App.java
@@ -1,22 +1,17 @@
 package org.apache.logging.log4j.core;
 
+import tools.aqua.concolic.Tainting;
+import tools.aqua.concolic.Verifier;
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Marker;
-//import tools.aqua.concolic.Tainting;
-//import tools.aqua.concolic.Verifier;
-
-import java.io.IOException;
 
 public class App {
 
-    protected static final Logger parentLogger = LogManager.getLogger();
-
     public static void main( String[] args ) {
         String s = Verifier.nondetString();
-        if(s.equals("test"))
-        {
-            assert false;
-        }
+        Logger logger = LogManager.getLogger();
+        logger.info(s + "TEST");
     }
 }
